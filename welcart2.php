61e5bb97cc90f63d06f1f1661f0d4eef8918d8a5
nanbu-collne <nanbu@collne.com>
2015-07-05 15:59:58 +0900

    Update welcart2.php

--- welcart2.php
+++ welcart2.php
@@ -17,7 +17,7 @@
  * Plugin Name:       Welcart2
  * Plugin URI:        @TODO
  * Description:       @TODO
- * Version:           1.0.1507051
+ * Version:           1.0.1507055
  * Author:            Collne Inc.
  * Author URI:        http://www.collne.com/
  * Text Domain:       plugin-name-locale
